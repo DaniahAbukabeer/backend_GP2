c1a6673dcfa120d02e03e8e792ca0425e7018705
daniah abukabeer <daniahmabukabeer@gmail.com>
2023-10-28 01:31:06 +0300

    pharmacies main landing page and add new stock page initial page

---
+++ WebAppTry1/WebAppTry1/Controllers/PharmacyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTry1.Controllers
+{
+    public class PharmacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStock()
+        {
+            return View();
+        }
+    }
+}
